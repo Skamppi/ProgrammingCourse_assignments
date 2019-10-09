95f110c59aa4f5420438e8460aeea20c5170aee2
Skamppi <46809473+Skamppi@users.noreply.github.com>
2019-10-09 23:51:13 +0300

    Update

--- Assignment3-5/GameWebApi/Repositories/MongoDbRepository.cs
+++ Assignment3-5/GameWebApi/Repositories/MongoDbRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GameWebApi.Models;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 
@@ -13,39 +14,67 @@ namespace GameWebApi.Repositories
         IMongoDatabaseSettings _settings;
         IMongoDatabase _database;
 
-        private  IMongoCollection<Player> _players; // TODO: toimiiko näin?
+        //private IMongoCollection<Player> _players; // TODO: toimiiko näin? EI TOIMI
+
+        private  IMongoCollection<BsonDocument> _players; // TODO: toimiiko näin? TOIMII
+
         public MongoDbRepository(IMongoDatabaseSettings settings)
         {
             _settings = settings;
             var client = new MongoClient(settings.ConnectionString);
             _database = client.GetDatabase(settings.DatabaseName);
 
-            
-            _players = _database.GetCollection<Player>(settings.PlayersCollectionName);
 
-            BsonClassMap.RegisterClassMap<Type>(cm =>
-            {
-                cm.AutoMap();
-                cm.MapIdMember(c => c.GUID);
-            });
+            //_players = _database.GetCollection<Player>(settings.PlayersCollectionName);
+            //BsonClassMap.RegisterClassMap<Type>(cm =>
+            //{
+            //    cm.AutoMap();
+            //    cm.MapIdMember(c => c.GUID);
+            //});
+
+            _players = _database.GetCollection<BsonDocument>(settings.PlayersCollectionName);
+
         }
 
         public Task<Player> Create(NewPlayer player)
         {
-           // _players.InsertOne(player);
-            throw new NotImplementedException();
+            // _players.InsertOne(player);
+            var newGuid = Guid.NewGuid().ToString().ToUpper();
+            var document = new BsonDocument
+            {
+                { "Id", newGuid },
+                { "Name", player.Name },
+                { "Score", 0 },
+                { "Level", 0 },
+                { "Isbanned", "false" },
+                { "CreationTime", DateTime.Now.ToString() },
+            };
+
+            _players.InsertOne(document);
+            return null;
         }
 
         public Task<Item> CreateItem(Guid playerId, Item item)
         {
-            
+            // get player
+
+            // add items
+
             throw new NotImplementedException();
         }
 
         public Task<Player> Delete(Guid id)
         {
-           
-            var x = _players.DeleteOne(p => p.Id == id);
+            // MUST BE UPPERCASE
+            var sid = id.ToString().ToUpper();
+
+            //var filter = Builders<BsonDocument>.Filter.Eq("Id", "3A287E9D-3F0F-4A19-AF36-FFD1CB4C5B88");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Id", sid);
+            _players.DeleteOne(filter);
+
+            // DONT work, why????
+            //var x = _players.DeleteOne(p => p.Id == id);
             
             return null;
         }
@@ -56,20 +85,34 @@ namespace GameWebApi.Repositories
         }
 
         public Task<Player> Get(Guid id)
-        {
+        {          
+            var first = _players.Find(new BsonDocument()).FirstOrDefault();
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Id", "AE0DE1E1-62B7-45CC-88AB-D2B0730C56D7");
+
+            var document2 = _players.Find(filter).First();
+
+
+            // These dont work !!!!
             //var x = _players.Find<Player>(p => p.Id == id).FirstOrDefault();
-            var xx = _players.Find<Player>(p => p.Name == "jack bauer").FirstOrDefault();
+            //var xx = _players.Find<Player>(p => p.Name == "jack bauer").FirstOrDefault();
+
+            // TODO: must return Player
+            
             throw new NotImplementedException();
         }
 
         public Task<Player[]> GetAll()
         {
-            _players.Find(player => true).ToList();
+            var all = _players.Find(player => true).ToList();
             throw new NotImplementedException();
         }
 
         public Task<Item[]> GetAllItems(Guid playerId)
         {
+            // get player
+
+            // return items
             throw new NotImplementedException();
         }
 
@@ -81,12 +124,22 @@ namespace GameWebApi.Repositories
         public Task<Player> Modify(Guid id, ModifiedPlayer player)
         {
             //_players.ReplaceOne(p => p.Id == id, player);
-            throw new NotImplementedException();
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Id", id.ToString().ToUpper());
+            var update = Builders<BsonDocument>.Update.Set("Score", player.Score);
+
+            _players.UpdateOne(filter, update);
+
+            return null;
+            //return Task.FromResult(player);
         }
 
         public Task<Item> UpdateItem(Guid playerId, Item item)
         {
+
+
             throw new NotImplementedException();
+            
         }
     }
 }
